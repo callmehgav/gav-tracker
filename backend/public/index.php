17a73c91c918f652d1198377a9a05aba42f925b3
Gavin Anderson <ganderson96@live.com>
2025-12-12 22:40:45 -0500

    fix index

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,2 +1,13 @@
 <?php
-echo "HELLO FROM TRACKER!";
+$path = __DIR__ . '/../../frontend/web/build';
+
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = realpath($path . $request);
+
+// If file exists (JS, CSS, assets), serve it
+if ($file && strpos($file, realpath($path)) === 0 && is_file($file)) {
+    return readfile($file);
+}
+
+// Otherwise serve React root
+readfile($path . '/index.html');
